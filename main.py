575a98d6f0743bb79c1bdc087495075c6b6f310d
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-06-30 05:27:07 +0300

    Update main.py1112

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def trim_history(history, max_length=4096):
 async def get_gpt_response(chat_history):
     try:
         response = await g4f.ChatCompletion.create_async(
-            model="gpt-3.5-turbo",  # Используйте правильное имя модели
+            model="defolt",  # Используйте правильное имя модели
             messages=chat_history,
             provider=g4f.Provider.Liaobots  # Используйте указанного провайдера
         )
